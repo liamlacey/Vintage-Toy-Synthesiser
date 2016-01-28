327ba1ebbe0226811a08fbb04c06a8a6156cfb5f
Liam Lacey <liamlacey@hotmail.com>
2016-01-28 17:59:05 +0000

    Made a default vintageBrain.c

--- Code/BBB/VintageToySynthProject/VintageBrain/vintageBrain.c
+++ Code/BBB/VintageToySynthProject/VintageBrain/vintageBrain.c
@@ -21,67 +21,94 @@
 #include <errno.h>
 
 #define KEYBOARD_SERIAL_PATH "/dev/ttyO1"
- 
-int main (void) 
+
+//==========================================================
+//==========================================================
+//==========================================================
+//Function that sets up a serial port
+
+int SetupSerialPort (const char path[], speed_t speed, bool should_be_blocking)
 {
-  printf ("Running test_key_mech_input (v2)...\n");
-
-  int keyboard_fd;
-  uint8_t keyboard_input_buf[1] = {0};
-  
-  //==========================================================
-  //Set up serial connection
-  
-  printf ("Setting up key mech serial connection...\n");
-
-  struct termios tty_attributes;
+    int fd;
+    struct termios tty_attributes;
     
-  // open UART1 device file for read/write
-  keyboard_fd = open (KEYBOARD_SERIAL_PATH, O_RDWR);
-  
-  //if can't open file
-  if (keyboard_fd < 0)
-  {
-    //show error and exit
-    perror (KEYBOARD_SERIAL_PATH);
-    return (-1);
-  }
-  
-  tcgetattr (keyboard_fd, &tty_attributes);
-  cfmakeraw (&tty_attributes);
-  tty_attributes.c_cc[VMIN]=1;
-  tty_attributes.c_cc[VTIME]=0;
-  
-  // setup bauds (key mech Arduino uses 38400)
-  cfsetispeed (&tty_attributes, B38400);
-  cfsetospeed (&tty_attributes, B38400);
-  
-  // apply changes now
-  tcsetattr (keyboard_fd, TCSANOW, &tty_attributes);
-  
-  // set it to blocking
-  fcntl (keyboard_fd, F_SETFL, 0);
-
-  //==========================================================
-  //Enter main loop, and just read any data that comes in over the serial port
-  
-  printf ("Starting reading data from key mech...\n");
-  
-  while (true)
-  {
-    //attempt to read a byte from the serial device file
-    int ret = read (keyboard_fd, keyboard_input_buf, 1);
-
-    //if read something
-    if (ret != -1)
+    // open device for read/write
+    fd = open (path, O_RDWR);
+    
+    //if can't open file
+    if (fd < 0)
     {
-      //display the read byte
-      printf ("Byte read from keyboard: %d\n", keyboard_input_buf[0]);
-     
-   } //if (ret)
+        //show error and exit
+        perror (path);
+        return (-1);
+    }
     
-  } ///while (true)
+    tcgetattr(fd,&tty_attributes);
+    cfmakeraw(&tty_attributes);
+    tty_attributes.c_cc[VMIN]=1;
+    tty_attributes.c_cc[VTIME]=0;
+    
+    // setup bauds
+    cfsetispeed (&tty_attributes, speed);
+    cfsetospeed (&tty_attributes, speed);
 
-  return 0;
+    // apply changes now
+    tcsetattr(fd, TCSANOW, &tty_attributes);
+    
+    if (should_be_blocking)
+    {
+        // set it to blocking
+        fcntl(fd, F_SETFL, 0);
+    }
+    else
+    {
+        // set it to non-blocking
+        fcntl(fd, F_SETFL, O_NONBLOCK);
+    }
+    
+    return fd;
+}
 
+//==========================================================
+//==========================================================
+//==========================================================
+//main function
+
+int main (void)
+{
+    printf ("Running vintageBrain...\n");
+    
+    int keyboard_fd;
+    uint8_t keyboard_input_buf[1] = {0};
+    
+    //==========================================================
+    //Set up serial connection
+    
+    printf ("Setting up key mech serial connection...\n");
+    
+    //open UART1 device file for read/write with baus rate of 38400
+    keyboard_fd = SetupSerialPort (KEYBOARD_SERIAL_PATH, B38400, true);
+    
+    //==========================================================
+    //Enter main loop, and just read any data that comes in over the serial port
+    
+    printf ("Starting reading data from key mech...\n");
+    
+    while (true)
+    {
+        //attempt to read a byte from the serial device file
+        int ret = read (keyboard_fd, keyboard_input_buf, 1);
+        
+        //if read something
+        if (ret != -1)
+        {
+            //display the read byte
+            printf ("Byte read from keyboard: %d\n", keyboard_input_buf[0]);
+            
+        } //if (ret)
+        
+    } ///while (true)
+    
+    return 0;
+    
 }
--- Code/BBB/VintageToySynthProject/copy_files_to_bbb.sh
+++ Code/BBB/VintageToySynthProject/copy_files_to_bbb.sh
@@ -3,6 +3,6 @@
 # This will includes the compiled vintageBrain binary, as well as all needed source files for compiling vintageSoundEngine on the BBB.
 
 echo "Copying files to the BB with IP address 192.168.7.2"
-scp -r VintageBrain/* root@192.168.7.2:/usr/VintageToySynthProject/VintageBrain/
-scp -r VintageSoundEngine/* root@192.168.7.2:/usr/VintageToySynthProject/VintageSoundEngine/
+scp -r "/Users/Liam/Documents/Vintage toy synth project/Code/BBB/VintageToySynthProject/VintageBrain/"* root@192.168.7.2:/usr/VintageToySynthProject/VintageBrain/
+scp -r "/Users/Liam/Documents/Vintage toy synth project/Code/BBB/VintageToySynthProject/VintageSoundEngine/"* root@192.168.7.2:/usr/VintageToySynthProject/VintageSoundEngine/
 
