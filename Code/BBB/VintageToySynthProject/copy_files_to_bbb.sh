7719d51e37b7d3df4b0f98ebfd6e5988b63eb5e5
Liam Lacey <liamlacey@hotmail.com>
2016-01-28 18:20:32 +0000

    Created init.d script for starting the vintageBrain application, as well as start and stop scripts for starting/stopping all needed applications. I haven't yet moved this file into /etc/init.d yet. Is this all I need to do to get it to start on boot? I don't think so - I think I somehow need to get start to be called.

--- Code/BBB/VintageToySynthProject/copy_files_to_bbb.sh
+++ Code/BBB/VintageToySynthProject/copy_files_to_bbb.sh
@@ -5,4 +5,6 @@
 echo "Copying files to the BB with IP address 192.168.7.2"
 scp -r "/Users/Liam/Documents/Vintage toy synth project/Code/BBB/VintageToySynthProject/VintageBrain/"* root@192.168.7.2:/usr/VintageToySynthProject/VintageBrain/
 scp -r "/Users/Liam/Documents/Vintage toy synth project/Code/BBB/VintageToySynthProject/VintageSoundEngine/"* root@192.168.7.2:/usr/VintageToySynthProject/VintageSoundEngine/
+scp -r "/Users/Liam/Documents/Vintage toy synth project/Code/BBB/VintageToySynthProject/start" root@192.168.7.2:/usr/VintageToySynthProject/
+scp -r "/Users/Liam/Documents/Vintage toy synth project/Code/BBB/VintageToySynthProject/stop" root@192.168.7.2:/usr/VintageToySynthProject/
 
