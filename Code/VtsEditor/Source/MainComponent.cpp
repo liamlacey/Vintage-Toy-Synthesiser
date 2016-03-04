26aa508946c52866723a7ec43252cc4980bda731
Liam Lacey <liamlacey@hotmail.com>
2016-03-04 15:00:44 +0000

    Implemented basic functionality for saving and loading patches for the synth.

--- Code/VtsEditor/Source/MainComponent.cpp
+++ Code/VtsEditor/Source/MainComponent.cpp
@@ -11,26 +11,283 @@
 
 //==============================================================================
 MainContentComponent::MainContentComponent()
+                    :   timeSliceThread ("contents list thread")
 {
-    setSize (600, 400);
+    //backend...
+    
+    timeSliceThread.startThread();
+    
+    patchDir = File::getSpecialLocation(File::userDocumentsDirectory).getFullPathName() + File::separatorString + "Vintage Toy Synthesiser" + File::separatorString + "patches";
+    
+    //if the patch directory doesn't yet exist, create it
+    if (patchDir.isDirectory() == false)
+    {
+        patchDir.createDirectory();
+    }
+    
+    //init audio device manager
+    audioDeviceManager.initialise (0, 0, NULL, true);
+    
+    //attach this class as the MIDI input callback to the audio device manager
+    audioDeviceManager.addMidiInputCallback(String::empty, this);
+    
+    for (int i = 0; i < 127; i++)
+        patchData[i] = -1;
+    
+    patchFileFilter = new WildcardFileFilter ("*", String::empty, "All files");
+    
+    patchFileDirContentsList = new DirectoryContentsList (patchFileFilter, timeSliceThread);
+    patchFileDirContentsList->setDirectory (patchDir, false, true);
+    
+    std::cout << patchFileDirContentsList->getDirectory().getFullPathName() << std::endl;
+    std::cout << patchFileDirContentsList->getNumFiles() << std::endl;
+    
+    
+    //GUI
+    addAndMakeVisible (audioDeviceSelectorComponent = new AudioDeviceSelectorComponent (audioDeviceManager, 0, 0, 0, 0, true, true, false, false));
+    
+    addAndMakeVisible(requestPatchDataButton = new TextButton());
+    requestPatchDataButton->addListener(this);
+    requestPatchDataButton->setButtonText("Get Patch Data From Synth");
+    
+    
+    addAndMakeVisible (patchNameEditor = new Label("patch name editor", "insert patch name"));
+    patchNameEditor->setEditable(true);
+    patchNameEditor->setColour(Label::backgroundColourId, Colours::white);
+    
+    addAndMakeVisible(saveButton = new TextButton());
+    saveButton->addListener(this);
+    saveButton->setButtonText("Save");
+    
+    addAndMakeVisible(patchDirFileListComponent = new FileListComponent(*patchFileDirContentsList));
+    patchDirFileListComponent->setColour(FileListComponent::backgroundColourId, Colours::white);
+    patchDirFileListComponent->addListener(this);
+    
+    addAndMakeVisible(eventLabel = new Label());
+    eventLabel->setText("Application started", dontSendNotification);
+    eventLabel->setJustificationType(Justification::right);
+    
+
+    setSize (800, 600);
 }
 
+//==============================================================================
 MainContentComponent::~MainContentComponent()
 {
+    
 }
 
-void MainContentComponent::paint (Graphics& g)
-{
-    g.fillAll (Colour (0xff001F36));
-
-    g.setFont (Font (16.0f));
-    g.setColour (Colours::white);
-    g.drawText ("Hello World!", getLocalBounds(), Justification::centred, true);
-}
-
+//==============================================================================
 void MainContentComponent::resized()
 {
-    // This is called when the MainContentComponent is resized.
-    // If you add any child components, this is where you should
-    // update their positions.
+    requestPatchDataButton->setBounds(0, 0, getWidth(), 20);
+    patchNameEditor->setBounds(0, 20, getWidth() - 50, 20);
+    saveButton->setBounds(getWidth()-50, 20, 50, 20);
+    
+    patchDirFileListComponent->setBounds(0, getHeight()/2, getWidth()/2, getHeight()/2);
+    audioDeviceSelectorComponent->setBounds(getWidth()/2, getHeight()/2, getWidth()/2, getHeight()/2);
+    
+    eventLabel->setBounds(getWidth()/2, getHeight()-20, getWidth()/2, 20);
+    
 }
+
+//==============================================================================
+void MainContentComponent::paint (Graphics& g)
+{
+    g.fillAll (Colours::grey);
+}
+
+//==============================================================================
+void MainContentComponent::buttonClicked (Button *button)
+{
+    if (button == saveButton)
+    {
+        savePatchFile();
+    }
+    
+    else if (button == requestPatchDataButton)
+    {
+        printf("Requesting patch data from synth\r\n");
+        eventLabel->setText("Requesting patch data from synth...", dontSendNotification);
+        
+        //TODO: send a specific command CC to the synth via MIDI to request it to send back all patch data.
+        //The synth then needs to send back a different command CC at the end which this application uses to flag that all patch data has vee received
+        
+    } //else if (button == requestPatchDataButton)
+}
+
+//==============================================================================
+void MainContentComponent::selectionChanged()
+{
+    
+}
+
+void MainContentComponent::fileClicked (const File &file, const MouseEvent &e)
+{
+    
+}
+
+void MainContentComponent::fileDoubleClicked (const File &file)
+{
+    loadPatchFile (file);
+}
+
+void MainContentComponent::browserRootChanged (const File &newRoot)
+{
+    
+}
+
+//==============================================================================
+void MainContentComponent::handleIncomingMidiMessage (MidiInput *source, const MidiMessage &message)
+{
+//    //debugging raw message
+//    const int message_size = message.getRawDataSize();
+//    const uint8_t* raw_message_buffer = message.getRawData();
+//
+//    printf("Incoming MIDI Message: ");
+//    
+//    for (int i = 0; i < message_size; i++)
+//    {
+//         printf("%d ", raw_message_buffer[i]);
+//    }
+//    
+//    printf("\r\n");
+    
+    if (message.isController())
+    {
+        printf("CC %d of value %d received\r\n", message.getControllerNumber(), message.getControllerValue());
+        
+        //if a patch CC (not CC 127 which is used for commands instead)
+        if (message.getControllerNumber() < 127)
+        {
+            //store the CC value
+            patchData[message.getControllerNumber()] = message.getControllerValue();
+            
+            MessageManagerLock mmlock;
+            String output_string = "Received CC num ";
+            output_string += message.getControllerNumber();
+            output_string += " with value ";
+            output_string += message.getControllerValue();
+            eventLabel->setText(output_string, dontSendNotification);
+            
+        } //if (message.getControllerNumber() < 127)
+        
+    } //if (message.isController())
+}
+
+//==============================================================================
+void MainContentComponent::sendMidiMessage (MidiMessage midiMessage)
+{
+    //If midi output exists (it won't if the user hasn't chosen an output device...)
+    if (audioDeviceManager.getDefaultMidiOutput())
+    {
+        audioDeviceManager.getDefaultMidiOutput()->startBackgroundThread();
+        audioDeviceManager.getDefaultMidiOutput()->sendBlockOfMessages(MidiBuffer(midiMessage), Time::getMillisecondCounter(), 44100);
+    }
+    else
+    {
+        std::cout << "A MIDI output device doesn't not exist!" << std::endl;
+        eventLabel->setText("Please select a MIDI-out!", dontSendNotification);
+    }
+
+}
+
+//==============================================================================
+void MainContentComponent::savePatchFile()
+{
+    std::cout << "Saving patch file with name: " << patchNameEditor->getText() << std::endl;
+    
+    eventLabel->setText("Saving patch to file...", dontSendNotification);
+    
+    //create patch file object
+    File patch_file = patchDir.getFullPathName() + File::separatorString + patchNameEditor->getText()/* + ".vts"*/;
+    
+    //create string for holding the contents of the file
+    String file_contents_string;
+    
+    //for each patch parameter
+    for (int i = 0; i < 127; i++)
+    {
+        //put into the following format on it's on line: [param num]:[param value]
+        file_contents_string += i;
+        file_contents_string += ":";
+        file_contents_string += patchData[i];
+        file_contents_string += "\n";
+    }
+    
+    //add string to file
+    patch_file.replaceWithText (file_contents_string);
+    
+    //create the file
+    patch_file.create();
+    
+    patchFileDirContentsList->refresh();
+    
+    eventLabel->setText("Finished saving patch", dontSendNotification);
+}
+
+//==============================================================================
+//==============================================================================
+//==============================================================================
+
+void MainContentComponent::loadPatchFile (const File file)
+{
+    std::cout << "Loading patch file: " << file.getFileName() << std::endl;
+    
+    eventLabel->setText("Loading patch and sending to synth...", dontSendNotification);
+    
+    //load the contents of the file into a string
+    String file_contents_string = file.loadFileAsString();
+    
+    //for each patch parameter
+    for (int i = 0; i < 127; i++)
+    {
+        //find the parameter number in the file contents...
+        
+        String param_num_string(i);
+        param_num_string += ":";
+        //find the start index of the param num string
+        int param_num_start_index = file_contents_string.indexOf (0, param_num_string);
+        
+        //if found the parameter number
+        if (param_num_start_index != -1)
+        {
+            //std::cout << "Found " << param_num_string << " at start index " << param_num_start_index << std::endl;
+            
+            //get the end index of the param num string value
+            int param_num_end_index = file_contents_string.indexOf (param_num_start_index, "\n");
+            
+            if (param_num_end_index != -1)
+            {
+                //std::cout << "Found " << param_num_string << " at end index " << param_num_end_index << std::endl;
+                
+                //get a substring of the contents based on the start and end indexes
+                String param_val_string = file_contents_string.substring (param_num_start_index, param_num_end_index);
+                //get the parameter value out of the substring
+                param_val_string = param_val_string.fromFirstOccurrenceOf(":", false, true);
+                
+                std::cout << "Param number " << i << " has a value of " << param_val_string << std::endl;
+                
+                //put the parameter value into the patch data array
+                
+                patchData[i] = param_val_string.getIntValue();
+                
+                //send the parameter value to the synth, if a valid value
+                if (patchData[i] >= 0 && patchData[i] <= 127)
+                {
+                    MidiMessage midi_message = MidiMessage::controllerEvent(1, i, patchData[i]);
+                    sendMidiMessage (midi_message);
+                    
+                } //if (patchData[i] >= 0 && patchData[i] <= 127)
+                
+            } //if (param_num_end_index != -1)
+
+        } //if (param_num_index != -1)
+
+    } //for (int i = 0; i < 127; i++)
+    
+    eventLabel->setText("Finished loading patch", dontSendNotification);
+}
+
+
+
--- Code/VtsEditor/Source/MainComponent.h
+++ Code/VtsEditor/Source/MainComponent.h
@@ -11,13 +11,19 @@
 
 #include "../JuceLibraryCode/JuceHeader.h"
 
+//FIXME: all backend stuff (MIDI IO, patch data storage, saving and loading files) should ideally be done in a seperate class to this one.
+//Right now I just want a quick and dirty working application though!
+
 
 //==============================================================================
 /*
     This component lives inside our window, and this is where you should put all
     your controls and content.
 */
-class MainContentComponent   : public Component
+class MainContentComponent   :  public Component,
+                                public MidiInputCallback,
+                                public Button::Listener,
+                                public FileBrowserListener
 {
 public:
     //==============================================================================
@@ -26,10 +32,48 @@ public:
 
     void paint (Graphics&);
     void resized();
+    
+    void buttonClicked (Button *button);
+    
+    void handleIncomingMidiMessage (MidiInput *source, const MidiMessage &message);
+    
+    //FileBrowserListener pure virtual callback functions
+    void selectionChanged();
+    void fileClicked (const File &file, const MouseEvent &e);
+    void fileDoubleClicked (const File &file);
+    void browserRootChanged (const File &newRoot);
+    
+    void sendMidiMessage (MidiMessage midiMessage);
+    
+    void savePatchFile ();
+    void loadPatchFile (const File patch_file);
 
 private:
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainContentComponent)
+    
+    //GUI
+    ScopedPointer <AudioDeviceSelectorComponent> audioDeviceSelectorComponent;
+    
+    ScopedPointer<TextButton> requestPatchDataButton;
+    
+    ScopedPointer<Label> patchNameEditor;
+    ScopedPointer<TextButton> saveButton;
+    ScopedPointer<FileListComponent> patchDirFileListComponent;
+    
+    ScopedPointer<Label> eventLabel;
+    
+    //backend
+    AudioDeviceManager audioDeviceManager;
+    
+    int patchData[127];
+    File patchDir;
+    
+    TimeSliceThread timeSliceThread;
+    
+    ScopedPointer <WildcardFileFilter> patchFileFilter;
+    ScopedPointer <DirectoryContentsList> patchFileDirContentsList;
+    
 };
 
 
