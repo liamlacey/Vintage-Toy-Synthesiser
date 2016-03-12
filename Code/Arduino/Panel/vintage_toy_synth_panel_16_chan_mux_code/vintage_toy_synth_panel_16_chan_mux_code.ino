a35d45dd0fc2bd7ff26d019ad44142ccdf75fee7
Liam Lacey <liamlacey@hotmail.com>
2016-03-12 16:50:23 +0000

    Auto-formatted panel code (cmd-t)

--- Code/Arduino/Panel/vintage_toy_synth_panel_16_chan_mux_code/vintage_toy_synth_panel_16_chan_mux_code.ino
+++ Code/Arduino/Panel/vintage_toy_synth_panel_16_chan_mux_code/vintage_toy_synth_panel_16_chan_mux_code.ino
@@ -40,7 +40,7 @@ const byte midiChan = 0;
 //==========================================
 //param data for each control
 
-struct ControlParamData 
+struct ControlParamData
 {
   const byte cc_num;
   const byte cc_min_val;
@@ -50,49 +50,49 @@ struct ControlParamData
 
 ControlParamData controlParamData[NUM_OF_CONTROLS] =
 {
-   {.cc_num = 74, .cc_min_val = 0, .cc_max_val = 127, false}, //0 - PARAM_FILTER_CUTOFF
-   {.cc_num = 19, .cc_min_val = 0, .cc_max_val = 127, false}, //1 - PARAM_FILTER_RESO
-   {.cc_num = 26, .cc_min_val = 0, .cc_max_val = 127, false}, //2 - PARAM_FILTER_LP_MIX
-   {.cc_num = 28, .cc_min_val = 0, .cc_max_val = 127, false}, //3 - PARAM_FILTER_HP_MIX
-   {.cc_num = 27, .cc_min_val = 0, .cc_max_val = 127, false}, //4 - PARAM_FILTER_BP_MIX
-   {.cc_num = 29, .cc_min_val = 0, .cc_max_val = 127, false}, //5 - PARAM_FILTER_NOTCH_MIX
-   {.cc_num = 50, .cc_min_val = 0, .cc_max_val = 3, false}, //6 - PARAM_LFO_SHAPE
-   {.cc_num = 47, .cc_min_val = 0, .cc_max_val = 127, false}, //7 - PARAM_LFO_RATE
-   {.cc_num = 48, .cc_min_val = 0, .cc_max_val = 127, true}, //8 - PARAM_LFO_DEPTH
-   {.cc_num = 14, .cc_min_val = 0, .cc_max_val = 127, false}, //9 - PARAM_OSC_SINE_LEVEL
-   {.cc_num = 15, .cc_min_val = 0, .cc_max_val = 127, false}, //10 - PARAM_OSC_TRI_LEVEL
-   {.cc_num = 16, .cc_min_val = 0, .cc_max_val = 127, false}, //11 - PARAM_OSC_SAW_LEVEL
-   {.cc_num = 18, .cc_min_val = 0, .cc_max_val = 127, false}, //12 - PARAM_OSC_SQUARE_LEVEL
-   {.cc_num = 17, .cc_min_val = 0, .cc_max_val = 127, false}, //13 - PARAM_OSC_PULSE_LEVEL
-   {.cc_num = 3, .cc_min_val = 0, .cc_max_val = 127, false}, //14 - PARAM_OSC_PULSE_AMOUNT
-   {.cc_num = 7, .cc_min_val = 0, .cc_max_val = 127, false}, //15 - PARAM_AEG_AMOUNT
-   {.cc_num = 73, .cc_min_val = 0, .cc_max_val = 127, false}, //16 - PARAM_AEG_ATTACK
-   {.cc_num = 75, .cc_min_val = 0, .cc_max_val = 127, false}, //17 - PARAM_AEG_DECAY
-   {.cc_num = 79, .cc_min_val = 0, .cc_max_val = 127, false}, //18 - PARAM_AEG_SUSTAIN
-   {.cc_num = 72, .cc_min_val = 0, .cc_max_val = 127, false}, //19 - PARAM_AEG_RELEASE
-   {.cc_num = 13, .cc_min_val = 0, .cc_max_val = 127, false}, //20 - PARAM_FX_DISTORTION_AMOUNT
-   {.cc_num = 33, .cc_min_val = 40, .cc_max_val = 88, false}, //21 - PARAM_OSC_SINE_NOTE
-   {.cc_num = 34, .cc_min_val = 40, .cc_max_val = 88, false}, //22 - PARAM_OSC_TRI_NOTE
-   {.cc_num = 35, .cc_min_val = 40, .cc_max_val = 88, false}, //23 - PARAM_OSC_SAW_NOTE
-   {.cc_num = 37, .cc_min_val = 40, .cc_max_val = 88, false}, //24 - PARAM_OSC_SQUARE_NOTE
-   {.cc_num = 36, .cc_min_val = 40, .cc_max_val = 88, false}, //25 - PARAM_OSC_PULSE_NOTE
-   {.cc_num = 20, .cc_min_val = 0, .cc_max_val = 127, false}, //26 - PARAM_OSC_PHASE_SPREAD
-   {.cc_num = 22, .cc_min_val = 0, .cc_max_val = 127, false}, //27 - PARAM_FEG_ATTACK
-   {.cc_num = 23, .cc_min_val = 0, .cc_max_val = 127, false}, //28 - PARAM_FEG_DECAY
-   {.cc_num = 24, .cc_min_val = 0, .cc_max_val = 127, false}, //29 - PARAM_FEG_SUSTAIN
-   {.cc_num = 25, .cc_min_val = 0, .cc_max_val = 127, false}, //30 - PARAM_FEG_RELEASE
-   {.cc_num = 107, .cc_min_val = 0, .cc_max_val = 127, false}, //31 - PARAM_GLOBAL_VINTAGE_AMOUNT
-   {.cc_num = 102, .cc_min_val = 0, .cc_max_val = 7, false}, //32 - PARAM_KEYS_SCALE
-   {.cc_num = 114, .cc_min_val = 61, .cc_max_val = 67, false}, //33 - PARAM_KEYS_OCTAVE
-   {.cc_num = 106, .cc_min_val = 58, .cc_max_val = 70, false}, //34 - PARAM_KEYS_TRANSPOSE
-   {.cc_num = 103, .cc_min_val = 0, .cc_max_val = 127, false}, //35 - PARAM_VOICE_MODE
-   {.cc_num = 58, .cc_min_val = 0, .cc_max_val = 127, true}, //36 - PARAM_MOD_LFO_AMP
-   {.cc_num = 112, .cc_min_val = 0, .cc_max_val = 127, true}, //37 - PARAM_MOD_LFO_CUTOFF
-   {.cc_num = 56, .cc_min_val = 0, .cc_max_val = 127, true}, //38 - PARAM_MOD_LFO_RESO
-   {.cc_num = 9, .cc_min_val = 0, .cc_max_val = 100, false}, //39 - PARAM_GLOBAL_VOLUME
-   {.cc_num = 63, .cc_min_val = 0, .cc_max_val = 127, true}, //40 - PARAM_MOD_VEL_AMP
-   {.cc_num = 109, .cc_min_val = 0, .cc_max_val = 127, true}, //41 - PARAM_MOD_VEL_CUTOFF
-   {.cc_num = 110, .cc_min_val = 0, .cc_max_val = 127, true}, //42 - PARAM_MOD_VEL_RESO
+  {.cc_num = 74, .cc_min_val = 0, .cc_max_val = 127, false}, //0 - PARAM_FILTER_CUTOFF
+  {.cc_num = 19, .cc_min_val = 0, .cc_max_val = 127, false}, //1 - PARAM_FILTER_RESO
+  {.cc_num = 26, .cc_min_val = 0, .cc_max_val = 127, false}, //2 - PARAM_FILTER_LP_MIX
+  {.cc_num = 28, .cc_min_val = 0, .cc_max_val = 127, false}, //3 - PARAM_FILTER_HP_MIX
+  {.cc_num = 27, .cc_min_val = 0, .cc_max_val = 127, false}, //4 - PARAM_FILTER_BP_MIX
+  {.cc_num = 29, .cc_min_val = 0, .cc_max_val = 127, false}, //5 - PARAM_FILTER_NOTCH_MIX
+  {.cc_num = 50, .cc_min_val = 0, .cc_max_val = 3, false}, //6 - PARAM_LFO_SHAPE
+  {.cc_num = 47, .cc_min_val = 0, .cc_max_val = 127, false}, //7 - PARAM_LFO_RATE
+  {.cc_num = 48, .cc_min_val = 0, .cc_max_val = 127, true}, //8 - PARAM_LFO_DEPTH
+  {.cc_num = 14, .cc_min_val = 0, .cc_max_val = 127, false}, //9 - PARAM_OSC_SINE_LEVEL
+  {.cc_num = 15, .cc_min_val = 0, .cc_max_val = 127, false}, //10 - PARAM_OSC_TRI_LEVEL
+  {.cc_num = 16, .cc_min_val = 0, .cc_max_val = 127, false}, //11 - PARAM_OSC_SAW_LEVEL
+  {.cc_num = 18, .cc_min_val = 0, .cc_max_val = 127, false}, //12 - PARAM_OSC_SQUARE_LEVEL
+  {.cc_num = 17, .cc_min_val = 0, .cc_max_val = 127, false}, //13 - PARAM_OSC_PULSE_LEVEL
+  {.cc_num = 3, .cc_min_val = 0, .cc_max_val = 127, false}, //14 - PARAM_OSC_PULSE_AMOUNT
+  {.cc_num = 7, .cc_min_val = 0, .cc_max_val = 127, false}, //15 - PARAM_AEG_AMOUNT
+  {.cc_num = 73, .cc_min_val = 0, .cc_max_val = 127, false}, //16 - PARAM_AEG_ATTACK
+  {.cc_num = 75, .cc_min_val = 0, .cc_max_val = 127, false}, //17 - PARAM_AEG_DECAY
+  {.cc_num = 79, .cc_min_val = 0, .cc_max_val = 127, false}, //18 - PARAM_AEG_SUSTAIN
+  {.cc_num = 72, .cc_min_val = 0, .cc_max_val = 127, false}, //19 - PARAM_AEG_RELEASE
+  {.cc_num = 13, .cc_min_val = 0, .cc_max_val = 127, false}, //20 - PARAM_FX_DISTORTION_AMOUNT
+  {.cc_num = 33, .cc_min_val = 40, .cc_max_val = 88, false}, //21 - PARAM_OSC_SINE_NOTE
+  {.cc_num = 34, .cc_min_val = 40, .cc_max_val = 88, false}, //22 - PARAM_OSC_TRI_NOTE
+  {.cc_num = 35, .cc_min_val = 40, .cc_max_val = 88, false}, //23 - PARAM_OSC_SAW_NOTE
+  {.cc_num = 37, .cc_min_val = 40, .cc_max_val = 88, false}, //24 - PARAM_OSC_SQUARE_NOTE
+  {.cc_num = 36, .cc_min_val = 40, .cc_max_val = 88, false}, //25 - PARAM_OSC_PULSE_NOTE
+  {.cc_num = 20, .cc_min_val = 0, .cc_max_val = 127, false}, //26 - PARAM_OSC_PHASE_SPREAD
+  {.cc_num = 22, .cc_min_val = 0, .cc_max_val = 127, false}, //27 - PARAM_FEG_ATTACK
+  {.cc_num = 23, .cc_min_val = 0, .cc_max_val = 127, false}, //28 - PARAM_FEG_DECAY
+  {.cc_num = 24, .cc_min_val = 0, .cc_max_val = 127, false}, //29 - PARAM_FEG_SUSTAIN
+  {.cc_num = 25, .cc_min_val = 0, .cc_max_val = 127, false}, //30 - PARAM_FEG_RELEASE
+  {.cc_num = 107, .cc_min_val = 0, .cc_max_val = 127, false}, //31 - PARAM_GLOBAL_VINTAGE_AMOUNT
+  {.cc_num = 102, .cc_min_val = 0, .cc_max_val = 7, false}, //32 - PARAM_KEYS_SCALE
+  {.cc_num = 114, .cc_min_val = 61, .cc_max_val = 67, false}, //33 - PARAM_KEYS_OCTAVE
+  {.cc_num = 106, .cc_min_val = 58, .cc_max_val = 70, false}, //34 - PARAM_KEYS_TRANSPOSE
+  {.cc_num = 103, .cc_min_val = 0, .cc_max_val = 127, false}, //35 - PARAM_VOICE_MODE
+  {.cc_num = 58, .cc_min_val = 0, .cc_max_val = 127, true}, //36 - PARAM_MOD_LFO_AMP
+  {.cc_num = 112, .cc_min_val = 0, .cc_max_val = 127, true}, //37 - PARAM_MOD_LFO_CUTOFF
+  {.cc_num = 56, .cc_min_val = 0, .cc_max_val = 127, true}, //38 - PARAM_MOD_LFO_RESO
+  {.cc_num = 9, .cc_min_val = 0, .cc_max_val = 100, false}, //39 - PARAM_GLOBAL_VOLUME
+  {.cc_num = 63, .cc_min_val = 0, .cc_max_val = 127, true}, //40 - PARAM_MOD_VEL_AMP
+  {.cc_num = 109, .cc_min_val = 0, .cc_max_val = 127, true}, //41 - PARAM_MOD_VEL_CUTOFF
+  {.cc_num = 110, .cc_min_val = 0, .cc_max_val = 127, true}, //42 - PARAM_MOD_VEL_RESO
 };
 
 //FOR DEVELOPMENT
@@ -100,8 +100,8 @@ ControlParamData controlParamData[NUM_OF_CONTROLS] =
 //{
 //
 //   {.cc_num = 0, .cc_min_val = 0, .cc_max_val = 127},
-//   {.cc_num = 1, .cc_min_val = 0, .cc_max_val = 127}, 
-//   {.cc_num = 2, .cc_min_val = 0, .cc_max_val = 127}, 
+//   {.cc_num = 1, .cc_min_val = 0, .cc_max_val = 127},
+//   {.cc_num = 2, .cc_min_val = 0, .cc_max_val = 127},
 //   {.cc_num = 3, .cc_min_val = 0, .cc_max_val = 127},
 //   {.cc_num = 4, .cc_min_val = 0, .cc_max_val = 127},
 //   {.cc_num = 5, .cc_min_val = 0, .cc_max_val = 127},
@@ -146,7 +146,7 @@ ControlParamData controlParamData[NUM_OF_CONTROLS] =
 
 void setup()
 {
-  //Setup serial comms for sending MIDI messages to BBB. 
+  //Setup serial comms for sending MIDI messages to BBB.
   //We don't need to use the MIDI baud rate (31250) here, as we're sending the messages to a general
   //serial output rather than a MIDI-specific output.
   Serial.begin(38400);
@@ -169,7 +169,7 @@ void loop()
   byte input_to_read;
   byte mux_input_pin;
   byte first_select_pin;
-  
+
   //for each control
   for (byte control_num = FIRST_CONTROL; control_num <= LAST_CONTROL; control_num++)
   {
@@ -205,71 +205,71 @@ void loop()
       first_select_pin = A4;
     }
 
-      //select the input pin on the mux we want to read from, by splitting
-      //the mux input pin into bits and sending the bit values to mux select pins.   
-      int b0 = bitRead (mux_input_pin, 0);   
-      int b1 = bitRead (mux_input_pin, 1);     
-      int b2 = bitRead (mux_input_pin, 2); 
-      int b3 = bitRead (mux_input_pin, 3); 
-      digitalWrite (first_select_pin, b0);
-      digitalWrite (first_select_pin + 1, b1);
-      digitalWrite (first_select_pin + 2, b2);
-      digitalWrite (first_select_pin + 3, b3);
+    //select the input pin on the mux we want to read from, by splitting
+    //the mux input pin into bits and sending the bit values to mux select pins.
+    int b0 = bitRead (mux_input_pin, 0);
+    int b1 = bitRead (mux_input_pin, 1);
+    int b2 = bitRead (mux_input_pin, 2);
+    int b3 = bitRead (mux_input_pin, 3);
+    digitalWrite (first_select_pin, b0);
+    digitalWrite (first_select_pin + 1, b1);
+    digitalWrite (first_select_pin + 2, b2);
+    digitalWrite (first_select_pin + 3, b3);
 
-      //read the input value
-      int read_val = analogRead (input_to_read);
+    //read the input value
+    int read_val = analogRead (input_to_read);
 
-      //==========================================
-      //==========================================
-      //==========================================
-      //Process analogue control input...
+    //==========================================
+    //==========================================
+    //==========================================
+    //Process analogue control input...
 
-      //if the read control value is greater that +/-5 from the last value
-      //this is a quick dirty hack to prevent jitter
-      if ((read_val > prevAnalogueValue[control_num] + 5) || 
-          (read_val < prevAnalogueValue[control_num] - 5) || 
-          (read_val == 0 && prevAnalogueValue[control_num] != 0) ||
-          (read_val == 1023 && prevAnalogueValue[control_num] != 1023))
+    //if the read control value is greater that +/-5 from the last value
+    //this is a quick dirty hack to prevent jitter
+    if ((read_val > prevAnalogueValue[control_num] + 5) ||
+        (read_val < prevAnalogueValue[control_num] - 5) ||
+        (read_val == 0 && prevAnalogueValue[control_num] != 0) ||
+        (read_val == 1023 && prevAnalogueValue[control_num] != 1023))
+    {
+
+      // Serial.print(control_num);
+      // Serial.print(" ");
+      // Serial.println(read_val);
+
+      //store the value
+      prevAnalogueValue[control_num] = read_val;
+
+      //convert the control value into a param/MIDI CC value
+      byte param_val = ConvertControlValToParamVal (control_num);
+
+      //if this control is for a bipolar depth parameter
+      if (controlParamData[control_num].is_depth_param == true)
       {
+        //make sure the control definietly centres on the centre value of the parameter
+        //by setting a certain window around the centre value to be set to the centre value
 
-        // Serial.print(control_num);
-        // Serial.print(" ");
-        // Serial.println(read_val);
-        
+        if (param_val >= 63 && param_val <= 65)
+        {
+          param_val = 64;
+
+        } //if (param_val >= 63 && param_val <= 65)
+
+      } //if (controlParamData[control_num].is_bipolar_control == true)
+
+      //if the param val is different from the last param val
+      if (prevParamValue[control_num] != param_val)
+      {
         //store the value
-        prevAnalogueValue[control_num] = read_val;
+        prevParamValue[control_num] = param_val;
 
-        //convert the control value into a param/MIDI CC value 
-        byte param_val = ConvertControlValToParamVal (control_num); 
+        //Send the param value as a MIDI CC message
+        SendMidiMessage (0xB0 + midiChan, controlParamData[control_num].cc_num, prevParamValue[control_num]);
 
-        //if this control is for a bipolar depth parameter
-        if (controlParamData[control_num].is_depth_param == true)
-        {
-          //make sure the control definietly centres on the centre value of the parameter
-          //by setting a certain window around the centre value to be set to the centre value
+      } //if (prevParamValue[control_num] != param_val)
 
-          if (param_val >= 63 && param_val <= 65)
-          {
-            param_val = 64;
-            
-          } //if (param_val >= 63 && param_val <= 65)
-          
-        } //if (controlParamData[control_num].is_bipolar_control == true)
+    } //if (prevAnalogueValue[control_num] != read_val)
 
-        //if the param val is different from the last param val
-        if (prevParamValue[control_num] != param_val)
-        {
-          //store the value
-          prevParamValue[control_num] = param_val;
-
-          //Send the param value as a MIDI CC message
-          SendMidiMessage (0xB0 + midiChan, controlParamData[control_num].cc_num, prevParamValue[control_num]);
-        
-        } //if (prevParamValue[control_num] != param_val)
-        
-      } //if (prevAnalogueValue[control_num] != read_val)
-
-      delay (1);
+    delay (1);
 
   } //for (byte control_num; control_num < NUM_OF_CONTROLS; control_num++)
 
@@ -279,12 +279,12 @@ void loop()
   //Read serial input...
 
   //if there is something to read on the serial port
-  if (Serial.available()) 
+  if (Serial.available())
   {
-      Serial.println ("Received messages from serial input");
+    Serial.println ("Received messages from serial input");
 
-     byte midi_in_buf[64];
-      
+    byte midi_in_buf[64];
+
     int num_of_bytes = Serial.readBytes (midi_in_buf, 64);
 
     //if received a request for all panel control values
@@ -293,12 +293,12 @@ void loop()
       //send back all control values
       for (byte control_num = 0; control_num < NUM_OF_CONTROLS; control_num++)
       {
-          SendMidiMessage (0xB0 + midiChan, controlParamData[control_num].cc_num, prevParamValue[control_num]); 
+        SendMidiMessage (0xB0 + midiChan, controlParamData[control_num].cc_num, prevParamValue[control_num]);
       }
-      
+
     } //if (num_of_bytes == 3 && midi_in_buf[0] == 0xB0 && midi_in_buf[1] = 127 && midi_in_buf[2] == 1)
-    
-  } //if (Serial.available()) 
+
+  } //if (Serial.available())
 
 }
 
@@ -321,16 +321,16 @@ byte ConvertControlValToParamVal (byte control_num)
 //=====================================================
 //Sends a 3 byte MIDI message to the serial output
 
-void SendMidiMessage (byte cmd_byte, byte data_byte_1, byte data_byte_2) 
+void SendMidiMessage (byte cmd_byte, byte data_byte_1, byte data_byte_2)
 {
   byte buf[3] = {cmd_byte, data_byte_1, data_byte_2};
-  
+
   Serial.write (buf, 3);
-  
-//  Serial.print(buf[0]);
-//  Serial.print(" ");
-//  Serial.print(buf[1]);
-//  Serial.print(" ");
-//  Serial.println(buf[2]);
+
+  //  Serial.print(buf[0]);
+  //  Serial.print(" ");
+  //  Serial.print(buf[1]);
+  //  Serial.print(" ");
+  //  Serial.println(buf[2]);
 }
 
